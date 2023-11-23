3817b40f5f11d2618ecb05df1bc083f1dc94effc
DaniFdz <danifernandezzzzzz@gmail.com>
2023-11-23 23:29:16 +0100

    Separated kitty from terminal config

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,15 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  fonts.fontconfig.enable = true;
-
   home.packages = with pkgs; [
     zsh
     bat
     lsd
     xclip
     neovim
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
   
   programs.zsh = {
@@ -78,39 +75,4 @@
 
   programs.fzf.enable = true;
   programs.fzf.enableZshIntegration = true;
-
-  programs.kitty = {
-    enable = true;
-    theme = "Dracula";
-    font = {
-      name = "Fira code";
-      size = 11;
-    };
-    keybindings = {
-      "alt+left" = "neighboring_window left";
-      "alt+right" = "neighboring_window right";
-      "alt+up" = "neighboring_window up";
-      "alt+down" = "neighboring_window down";
-      "ctrl+shift+z" = "toggle_layout stack";
-      "ctrl+shift+enter" = "new_window_with_cmd";
-      "ctrl+shift+t" = "new_tab_with_cmd";
-    };
-    settings = {
-      enable_audio_bell = false;
-      placement_strategy = "center";
-      disable_ligatures = "never";
-      cursor_shape = "beam";
-      cursor_beam_thickness = "1.8";
-      mouse_hide_wait = "3.0";
-      background_opacity = "0.96";
-      detect_url = true;
-      repaint_delay = 10;
-      input_delay = 3;
-      sync_to_monitor = true;
-      inactive_tab_background = "#f7768e";
-      active_tab_background = "#bb9af7";
-      inactive_tab_foreground = "#000000";
-      tab_bar_margin_color = "black";
-    };
-  };
 }
