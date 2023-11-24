965a4e7ab02171b46d16d90ec940fe3d96b912c1
DaniFdz <danifernandezzzzzz@gmail.com>
2023-11-24 13:38:07 +0100

    Some plugins added to neovim config

--- common/nvim.nix
+++ common/nvim.nix
@@ -1,10 +1,40 @@
+{ config, pkgs, ... }:
+
 {
-  programs.neovim = {
+  programs.neovim = 
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  in
+	{
     enable = true;
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
 
+		plugins = with pkgs.vimPlugins; [
+		  # Look for packages: nix-env -f '<nixpkgs>' -qaP -A vimPlugins
+
+			{
+				plugin = dracula-nvim;
+				config = "colorscheme dracula";
+			}	
+
+      {
+        plugin = nvim-lspconfig;
+        config = toLuaFile ./nvim/plugin/lsp.lua;
+      }
+
+			{
+        plugin = comment-nvim;
+        config = toLua "require(\"Comment\").setup()";
+      }
+
+			nvim-web-devicons
+
+			vim-nix
+		];
+ 
     extraLuaConfig = ''
       ${builtins.readFile ./nvim/options.lua}
     '';
--- common/nvim/options.lua
+++ common/nvim/options.lua
@@ -8,11 +8,12 @@ vim.o.number = true
 
 vim.o.signcolumn = 'yes'
 
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 
 vim.o.updatetime = 300
 
 vim.o.termguicolors = true
 
 vim.o.mouse = 'a'
+-- vim.o.signcolumn = 'no'
