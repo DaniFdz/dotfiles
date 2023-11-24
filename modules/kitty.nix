2362d2288cb7f0f61cf6e5138f9136f3c83f2fb5
DaniFdz <danifernandezzzzzz@gmail.com>
2023-11-24 14:43:26 +0100

    Changed Dracula to Tokyo Night color scheme

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -12,7 +12,6 @@
 
   programs.kitty = {
     enable = true;
-    theme = "Dracula";
     font = {
       name = "Fira code";
       size = 11;
@@ -42,6 +41,44 @@
       active_tab_background = "#bb9af7";
       inactive_tab_foreground = "#000000";
       tab_bar_margin_color = "black";
+
+      # Theme
+      foreground = "#a9b1d6";
+      background = "#1a1b26";
+      #Black"
+      color0 = "#414868";
+      color8 = "#414868";
+      #Red"
+      color1 = "#f7768e";
+      color9 = "#f7768e";
+      #Green"
+      color2 = "#73daca";
+      color10 = "#73daca";
+      #Yellow"
+      color3 = "#e0af68";
+      color11 = "#e0af68";
+      #Blue"
+      color4 = "#7aa2f7";
+      color12 = "#7aa2f7";
+      #Magenta"
+      color5 = "#bb9af7";
+      color13 = "#bb9af7";
+      #Cyan"
+      color6 = "#7dcfff";
+      color14 = "#7dcfff";
+      #White"
+      color7 = "#c0caf5";
+      color15 = "#c0caf5";
+      cursor = "#c0caf5";
+      cursor_text_color = "#1a1b26";
+      #Selection = highlight"
+      selection_foreground = "none";
+      selection_background = "#28344a";
+      url_color = "#9ece6a";
+      #Window = borders"
+      active_border_color = "#3d59a1";
+      inactive_border_color = "#101014";
+      bell_border_color = "#e0af68";
     };
   };
 }
