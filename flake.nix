4fc2e86617a3898ba7d0f434a31e761e0bf783b4
DaniFdz <danifernandezzzzzz@gmail.com>
2023-11-25 12:15:18 +0100

    Removed unused inputs

--- flake.nix
+++ flake.nix
@@ -20,10 +20,6 @@
     };
 
     hyprland.url = "github:hyprwm/Hyprland";
-    hyprland-contrib = {
-      url = "github:hyprwm/contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
