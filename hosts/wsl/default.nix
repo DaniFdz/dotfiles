65624d9264f2a6ac795600220946dacb06b7b3f7
DaniFdz <danifernandezzzzzz@gmail.com>
2023-11-28 13:17:09 +0000

    Added vscode server

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -38,4 +38,17 @@
 
     system.stateVersion = "23.05";
 
+		systemd.user = {
+		paths.vscode-remote-workaround = {
+			wantedBy = ["default.target"];
+			pathConfig.PathChanged = "%h/.vscode-server/bin";
+		};
+
+		services.vscode-remote-workaround.script = ''
+			for i in ~/.vscode-server/bin/*; do
+				echo "Fixing vscode-server in $i..."
+				ln -sf ${pkgs.nodejs_18}/bin/node $i/node
+			done
+		'';
+		};
   }
