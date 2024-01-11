2e4a0a37b8cafe36bc63d7c633cc28cedb8e1978
DaniFdz <danifernandezzzzzz@gmail.com>
2024-01-11 10:23:14 +0100

    Fixed lsp capabilites

--- home/capabilities/nvim/plugins/lsp.lua
+++ home/capabilities/nvim/plugins/lsp.lua
@@ -4,6 +4,30 @@ vim.g.mapleader = " "
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
+
+local onrattach = function (_, bufnr)
+	local bufmap = function(shortcut, command)
+		vim.keymap.set("n", shortcut, command, { buffer = bufnr })
+	end
+
+	bufmap("gD", vim.lsp.buf.declaration)
+	bufmap("gd", vim.lsp.buf.definition)
+	bufmap("K", vim.lsp.buf.hover)
+	bufmap("gi", vim.lsp.buf.implementation)
+	bufmap("gr", vim.lsp.buf.references)
+	bufmap("<leader>k", vim.lsp.buf.signature_help)
+	bufmap("<leader>wa", vim.lsp.buf.add_workspace_folder)
+	bufmap("<leader>wr", vim.lsp.buf.remove_workspace_folder)
+	bufmap("<leader>wl", vim.lsp.buf.list_workspace_folders)
+	bufmap("<leader>D", vim.lsp.buf.type_definition)
+	bufmap("<leader>rn", vim.lsp.buf.rename)
+	bufmap("<leader>ca", vim.lsp.buf.code_action)
+	
+	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+		vim.lsp.buf.format()
+	end, {})
+end
+
 require("neodev").setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
@@ -80,31 +104,4 @@ lspconfig.rnix.setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
 })
-
-
-
-
-
-local on_attach = function (_, bufnr)
-	local bufmap = function(shortcut, command)
-		vim.keymap.set("n", shortcut, command, { buffer = bufnr })
-	end
-
-	bufmap("gD", vim.lsp.buf.declaration)
-	bufmap("gd", vim.lsp.buf.definition)
-	bufmap("K", vim.lsp.buf.hover)
-	bufmap("gi", vim.lsp.buf.implementation)
-	bufmap("gr", vim.lsp.buf.references)
-	bufmap("<leader>k", vim.lsp.buf.signature_help)
-	bufmap("<leader>wa", vim.lsp.buf.add_workspace_folder)
-	bufmap("<leader>wr", vim.lsp.buf.remove_workspace_folder)
-	bufmap("<leader>wl", vim.lsp.buf.list_workspace_folders)
-	bufmap("<leader>D", vim.lsp.buf.type_definition)
-	bufmap("<leader>rn", vim.lsp.buf.rename)
-	bufmap("<leader>a", vim.lsp.buf.code_action)
-	
-	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-		vim.lsp.buf.format()
-	end, {})
-end
 
