66f3bdcc0795ec2504455d35937c62ec25e43052
DaniFdz <danifernandezzzzzz@gmail.com>
2024-01-16 11:07:13 +0000

    Deleted useless rules

--- home/capabilities/nvim/plugins/harpoon.lua
+++ home/capabilities/nvim/plugins/harpoon.lua
@@ -4,14 +4,10 @@ vim.api.nvim_set_keymap("n", "<leader>hh", "<cmd>lua require('harpoon.ui').toggl
 	{ noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>",
 	{ noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>hd", "<cmd>lua require('harpoon.mark').remove_file()<cr>",
-	{ noremap = true, silent = true })
 for i = 1, 9 do
 	vim.api.nvim_set_keymap("n", "<leader>h" .. i, "<cmd>lua require('harpoon.ui').nav_file(" .. i .. ")<cr>",
 		{ noremap = true, silent = true })
 end
-vim.api.nvim_set_keymap("n", "<leader>hr", "<cmd>lua require('harpoon.ui').reset()<cr>",
-	{ noremap = true, silent = true })
 
 harpoon.setup({
 	global_settings = {
