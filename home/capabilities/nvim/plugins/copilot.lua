03e1bcfb1b5f661c35de21e768fc44913d14f54a
DaniFdz <danifernandezzzzzz@gmail.com>
2024-05-17 15:13:49 +0000

    Disable copilot by default

--- home/capabilities/nvim/plugins/copilot.lua
+++ home/capabilities/nvim/plugins/copilot.lua
@@ -1,6 +1,12 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		vim.cmd("silent! Copilot disable")
+	end,
+})
+
 vim.api.nvim_set_keymap("i", "<C-l>", "copilot#Accept('<CR>')", {expr=true, silent=true})
 vim.keymap.set("i", "<C-j>", "copilot#Next()", {expr=true, silent=true})
 vim.keymap.set("i", "<C-k>", "copilot#Previous()", {expr=true, silent=true})
